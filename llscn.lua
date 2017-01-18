4a4e4eb1f6ba8d6a0a67f9335f39f911931be9ff
moonhappy <phillipc@phillipcooper.id.au>
2017-01-18 23:17:19 +0800

    Updates to the cus management to make actors optionally obidient to specific directors.

--- llscn.lua
+++ llscn.lua
@@ -18,25 +18,21 @@ end
 
 function LnaActor:onCue(cueName, callbackName, director)
   self.cues[#self.cues + 1] = {cue=cueName, cb=callbackName, obj=self, dir=director}
-  if director then
-    director.cues[#director.cues + 1] = {cue=cueName, cb=callbackName, obj=self, dir=director}
-  elseif self.scene then
+  if self.scene then
     self.scene.cues[#self.scene.cues + 1] = {cue=cueName, cb=callbackName, obj=self, dir=self.scene}
   end
 end
 
 function LnaActor:onKeyboardCue(keyName, callbackName, director)
   self.kbCues[#self.kbCues + 1] = {key=keyName, cb=callbackName, obj=self, dir=director}
-  if director then
-    director.kbCues[#director.kbCues + 1] = {key=keyName, cb=callbackName, obj=self, dir=director}
-  elseif self.scene then
+  if self.scene then
     self.scene.kbCues[#self.scene.kbCues + 1] = {key=keyName, cb=callbackName, obj=self, dir=self.scene}
   end
 end
 
 function LnaActor:signalCue(cueName)
   if self.scene then
-    self.scene:signalCue(cueName)
+    self.scene:_signalCue(cueName, self)
   end
 end
 
@@ -57,20 +53,20 @@ function LnaActor:_setDirector(director)
   end
 end
 
-function LnaActor:_sceneSet(id, scene)
+function LnaActor:_setScene(id, scene)
   self.id = id
   self.scene = scene
   if scene then
     count = #self.cues
     for i=1,count do
-      if self.cues[i].dir == scene or not self.cues[i].dir then
-        director.cues[#director.cues+1] = self.cues[i]
+      if not self.cues[i].dir or self.cues[i].dir == scene then
+        scene.cues[#scene.cues+1] = self.cues[i]
       end
     end
     count = #self.kbCues
     for i=1,count do
-      if self.kbCues[i].dir == director or not self.kbCues[i].dir then
-        director.kbCues[#director.kbCues+1] = self.kbCues[i]
+      if not self.kbCues[i].dir or self.kbCues[i].dir == scene then
+        scene.kbCues[#scene.kbCues+1] = self.kbCues[i]
       end
     end
   end
@@ -112,7 +108,6 @@ end
 function LnaDirector:addActor(actor)
   if actor then
     self.actors[#self.actors + 1] = actor
-    actor:_setDirector(self)
     return #self.actors
   else
     return -1
@@ -121,6 +116,31 @@ end
 
 function LnaDirector:addDirector(director)
   return self:addActor(director)
+end
+
+function LnaDirector:_setScene(id, scene)
+  self.id = id
+  self.scene = scene
+  if scene then
+    -- cues from actors
+    local count = #self.actors
+    for i=1,count do
+      local childActor = self.actors[i]
+      local cueCount = #childActor.cues
+      for x=1,cueCount do
+        self.cues[#self.cues+1] = childActor.cues[x]
+      end
+    end
+    -- add cues to scene
+    count = #self.cues
+    for i=1,count do
+      scene.cues[#scene.cues+1] = self.cues[i]
+    end
+    count = #self.kbCues
+    for i=1,count do
+      scene.kbCues[#scene.kbCues+1] = self.kbCues[i]
+    end
+  end
 end
 
 --[[--
@@ -144,7 +164,7 @@ function LnaScene:_setAsCurrent()
   self.cues = {}
   -- Configure all actors
   for i,v in pairs(self.actors) do
-    v:_sceneSet(i, self)
+    v:_setScene(i, self)
   end
 end
 
@@ -158,8 +178,12 @@ function LnaScene:addDirector(director)
 end
 
 function LnaScene:signalCue(eventName)
+  self:_signalCue(eventName, self)
+end
+
+function LnaScene:_signalCue(eventName, obj)
   for i,v in pairs(self.cues) do
-    if v.cue == eventName and (not v.director or v.director.active) then
+    if v.cue == eventName and (not v.dir or (v.dir.active and v.dir == obj)) then
       v.obj[v.cb](v.obj)
     end
   end
@@ -167,7 +191,7 @@ end
 
 function LnaScene:signalKeyboardCue(key)
   for i,v in pairs(self.kbCues) do
-    if v.key == key and (not v.director or v.director.active) then
+    if v.key == key and (not v.dir or v.dir.active) then
       v.obj[v.cb](v.obj)
     end
   end
--- main.lua
+++ main.lua
@@ -23,6 +23,24 @@ function HiWorld:draw()
 end
 
 --[[
+  Another example actor.
+  However, this actor will only monitor cues issued from particular director.
+]]
+
+local ObidientActor = class("ObidientActor", Lna.Actor)
+function ObidientActor:initialize(director)
+  Lna.Actor.initialize(self)
+  self.message = "Director not saying anything yet"
+  self:onCue("director_says_hi", "sayHi", director)
+end
+function ObidientActor:sayHi()
+  self.message = "Director says hi"
+end
+function ObidientActor:draw()
+  love.graphics.print(self.message, 200, 200)
+end
+
+--[[
   Example director.
   Monitors the time and will signal the cue for Hello World actor to start
   performing the less famous "bye world" routine, after 3 seconds have passed.
@@ -32,12 +50,16 @@ function HiWorldDirector:initialize()
   Lna.Director.initialize(self)
   self.timePassed = 0
   self.cueFired = false
+  self.subCueFired = false
 end
 function HiWorldDirector:update(dt)
   self.timePassed = self.timePassed + dt
-  if not self.cureFired and self.timePassed > 3 then
+  if not self.cueFired and self.timePassed > 3 then
     self:signalCue("hiworld_bye")
     self.cueFired = true
+  elseif not self.subCueFired and self.timePassed > 4 then
+    self:signalCue("director_says_hi")
+    self.subCueFired = true
   end
 end
 
@@ -45,9 +67,11 @@ end
 local scene = Lna.Scene:new()
 local actor = HiWorld:new()
 local director = HiWorldDirector:new()
-local actorIdx = scene:addActor(actor)
+local actorIdx = scene:addActor(actor) -- note this actor is not managed by a director and will perform on global cues
+local obidientActor = ObidientActor:new(director)
+local obidientActorIdx = scene:addActor(obidientActor)
+-- local obidientActorDirIdx = director:addActor(obidientActor) -- uncomment to see director cue handled
 local directorIdx = scene:addDirector(director)
-
 local stage = Lna.Stage:new()
 local sceneIdx = stage:addScene(scene)
 stage:setCurrentScene(sceneIdx)
